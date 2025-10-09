6bdb3a0f017df1f678adcd328c68709bf7004755
Dhiraj jadhav <dhirajjadhav77777@gmail.com>
2025-10-10 01:33:59 +0530

    added the for loop repeatithon code

---
+++ REPEATITHON/SESSION-035/01-while-loop.py
@@ -0,0 +1,34 @@
+#Repeat 1
+L = [10, True, 20, False]
+i = 10
+while i<len(L):
+    print(i, L[i])
+    i = i + 1
+
+#Repeate 2
+L = [10, True, 20, False]
+i = 0
+while i<len(L):
+    print(i, L[i])
+    i = i+1
+
+#Repeat 3
+L = [10, True, 20, False]
+i = 0
+while i<len(L):
+    print(i, L[i])
+    i = i+1
+
+#Repeat 4
+L = [10, True, 20, False]
+i = 0
+while i< len(L):
+    print(i, L[i])
+    i = i + 1
+
+# Repeat 5
+L = [10, True, 20, False]
+i = 0
+while i< len(L):
+    print(i, L[i])
+    i = i+1
